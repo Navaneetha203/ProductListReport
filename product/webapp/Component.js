4cbc7d5c02cbb977cb1fff7997a8bc6b3772733d
Navaneetha Nalamada <navaneethanalamada@gmail.com>
2025-03-12 09:36:11 +0000

    inial setup

---
+++ product/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.mindset.listrepot.product.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
